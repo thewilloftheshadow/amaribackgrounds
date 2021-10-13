d7a4c8977c566c00de7d2a3f80eccc20eb379996
Shadow <thebotoftheshadow@gmail.com>
2021-10-12 22:14:07 -0500

    fix dir

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/api/*", function (req, res) {
   res.sendStatus(200)
 })
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 app.listen(process.env.PORT);
-console.log("AmariBackgrounds has been started!")
+console.log("AmariBackgrounds has been started, port " + process.env.PORT)
