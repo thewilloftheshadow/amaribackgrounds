27ea1bb5617734ae725b4a90693e93999104a6c9
Shadow <thebotoftheshadow@gmail.com>
2021-10-14 10:01:06 -0500

    k

--- client/src/pages/Backgrounds.js
+++ client/src/pages/Backgrounds.js
@@ -53,20 +53,4 @@ const Backgrounds = () => {
   )
 }
 
-const AllBackgrounds = () => {
-  return <div></div>
-}
-
-const Paginated = () => {
-  let commentNodes = this.props.data.map(function (comment, index) {
-    return <div key={index}>{comment.comment}</div>
-  })
-
-  return (
-    <div id="project-comments" className="commentList">
-      <ul>{commentNodes}</ul>
-    </div>
-  )
-}
-
 export default Backgrounds
